1779ddf286d574554af6836964f39dd107105d19
Afdhaluddin Afif Asha'ari <afdhaluddinafif@gmail.com>
2019-02-02 23:49:14 +0800

    Updated control structure

--- transactions.php
+++ transactions.php
@@ -4,8 +4,11 @@ include(CLASS_DIR.'class.transactions.php');
 $pageTitle = 'Transactions';
 include(LAYOUT_DIR.'header.php');
  
-if (!empty($_GET['action'])) {
-  include(INCLUDE_DIR.'transactions/add.php');
+if (isset(($_GET['action']))) {
+  $action = $_GET['action'];
+  if ($action == 'add') {
+    include(INCLUDE_DIR.'transactions/add.php');
+  }
 } else {
   include(INCLUDE_DIR.'transactions/view.php');
 }
