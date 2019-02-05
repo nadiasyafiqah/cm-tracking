84cc774694505a55a272d6584cc0342b8c146d6d
Afdhaluddin Afif Asha'ari <afdhaluddinafif@gmail.com>
2019-02-05 15:48:33 +0800

    Added pagination

--- inc/transactions/view.php
+++ inc/transactions/view.php
@@ -1,54 +1,72 @@
 <h1>Transations List</h1>
 <div class="table">
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Type</th>
-      <th scope="col">Date</th>
-      <th scope="col">Brand</th>
-      <th scope="col">Model</th>
-      <th scope="col">Serial</th>
-      <th scope="col">Location</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th>1</th>
-      <td>Check-in</td>
-      <td>23/11/2018</td>
-      <td>HP</td>
-      <td>8100 CMT</td>
-      <td>SGH78356</td>
-      <td>Johor | KK Batu Pahat</td>
-      <td><a href="$">Edit</a></td>
-    </tr>
-    <tr>
-      <th>2</th>
-      <td>Check-out</td>
-      <td>25/11/2018</td>
-      <td>HP</td>
-      <td>8100 CMT</td>
-      <td>SGH78356</td>
-      <td>Melaka | Politeknik Melaka</td>
-      <td><a href="$">Edit</a></td>
-    </tr>
-    <tr>
-      <th>3</th>
-      <td>Check-in</td>
-      <td>27/11/2018</td>
-      <td>Lenovo</td>
-      <td>T440</td>
-      <td>LN873411100</td>
-      <td>HQ | Prodata</td>
-      <td><a href="$">Edit</a></td>
-    </tr>
-  </tbody>
-</table>
-<div class="flex">
-  <div class="float-right">
-    <small>Total 3 transactions</small>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Type</th>
+        <th scope="col">Date</th>
+        <th scope="col">Brand</th>
+        <th scope="col">Model</th>
+        <th scope="col">Serial</th>
+        <th scope="col">Location</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>1</th>
+        <td>Check-in</td>
+        <td>23/11/2018</td>
+        <td>HP</td>
+        <td>8100 CMT</td>
+        <td>SGH78356</td>
+        <td>Johor | KK Batu Pahat</td>
+        <td><a href="$">Edit</a></td>
+      </tr>
+      <tr>
+        <th>2</th>
+        <td>Check-out</td>
+        <td>25/11/2018</td>
+        <td>HP</td>
+        <td>8100 CMT</td>
+        <td>SGH78356</td>
+        <td>Melaka | Politeknik Melaka</td>
+        <td><a href="$">Edit</a></td>
+      </tr>
+      <tr>
+        <th>3</th>
+        <td>Check-in</td>
+        <td>27/11/2018</td>
+        <td>Lenovo</td>
+        <td>T440</td>
+        <td>LN873411100</td>
+        <td>HQ | Prodata</td>
+        <td><a href="$">Edit</a></td>
+      </tr>
+    </tbody>
+  </table>
+  <div>
+    <div>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="float-right">
+        <small>Total 3 transactions</small>
+      </div>
+    </div>
+    <div class="flex">
+      
+    </div>
   </div>
-</div>
 </div>
