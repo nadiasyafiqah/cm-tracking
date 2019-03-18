2b41c49b00d5e3e9bc23dd20d3e751412740b7b0
Afdhaluddin Afif Asha'ari <afdhaluddinafif@gmail.com>
2019-03-18 11:51:43 +0800

    Add module selection page

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<?php include_once('config.php'); ?>
-
-<?php include(LAYOUT_DIR.'header.php'); ?>
-<h1>This will be the dashboard</h1>
-<?php include(LAYOUT_DIR.'footer.php'); ?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<div class="jumbotron">
+  <h1 class="display-4 text-center">Welcome to CM Tracking Sysytem</h1>
+  <p class="lead text-center">Please select one of the module below to proceed.</p>
+  <hr class="my-4">
+  <div class="row">
+    <div class="col text-center">
+      <a href="buffertrack/" class="btn btn-lg btn-info text-center">Buffer Set Tracking</a>
+    </div>
+    <div class="col text-center">
+    <a href="#" class="btn btn-lg btn-info text-center disabled">Spare Parts Inventory</a>
+    </div>
+  </div>
+  </p>
+</div>
