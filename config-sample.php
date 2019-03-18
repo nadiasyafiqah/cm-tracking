649946f5f909f1c62303a1267de8965b24623146
Afdhaluddin Afif Asha'ari <afdhaluddinafif@gmail.com>
2019-03-18 14:29:25 +0800

    Add sample config.php

---
+++ config-sample.php
@@ -0,0 +1,20 @@
+<?php
+//Database config
+$db_hostname = '';
+$db_username ='';
+$db_password = '';
+$db_name = '';
+
+$connection = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+
+if (!$connection) {
+  die('Database connection failed' . mysqli_error($connection));
+}
+
+//Dir setting
+define('ROOT', __DIR__.'/');
+define('INCLUDE_DIR', __DIR__.'/inc/');
+define('BT_INCLUDE_DIR', __DIR__.'/buffertrack/inc/');
+define('BT_LAYOUT_DIR', __DIR__.'/buffertrack/inc/layouts/');
+define('BT_CLASS_DIR', __DIR__.'/buffertrack/inc/class/');
+?>
