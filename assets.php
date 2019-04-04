7508d3b39514b6ad1fd3490d459f984c94a12bea
Afdhaluddin Afif Asha'ari <42729942+afdhal395@users.noreply.github.com>
2019-04-04 22:45:07 +0800

    initial commit

---
+++ assets.php
@@ -0,0 +1,3 @@
+<?php include('inc/layout/header.php'); ?>
+<?php include('inc/assets/view-sample.php'); ?>
+<?php include('inc/layout/footer.php'); ?>
---
+++ inc/layout/navbar.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php">Prodata CM Tracking Application</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" >Asset</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="assets.php?add">Add Asset</a>
+              <a class="dropdown-item" href="assets.php">Assets List</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" >Spare Parts</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="">Add Spareparts</a>
+              <a class="dropdown-item" href="">Spareparts List</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0" action="search.php">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search Serial" aria-label="Search">
+        </form>
+        <div>
+        <i class="fa fa-user fa-lg" aria-hidden="true"></i>
+        </div>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="profile.php">Hi, Admin</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main role="main" class="container">
