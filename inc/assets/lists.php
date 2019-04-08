8cd032f61c5b6c1dc01321f72d7e9bba8622a363
Afdhaluddin Afif Asha'ari <42729942+afdhal395@users.noreply.github.com>
2019-04-08 13:24:36 +0800

    Add asset list view

---
+++ inc/assets/lists.php
@@ -0,0 +1,15 @@
+<h1>Asset List</h1>
+<table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Brand</th>
+      <th scope="col">Model</th>
+      <th scope="col">Serial</th>
+      <th scope="col">Current Location</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php fetchAssetList(); ?>
+  </tbody>
+</table>
